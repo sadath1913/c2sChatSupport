475f18a8723e63b546e75728971e3503ffbae869
sadath1913 <sadathkhan717@gmail.com>
2026-08-30 12:10:15 +0530

    Added Frontend Deployed Url in backend Coors

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://c2s-chat-support.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
